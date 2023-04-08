f1f8554879f10984d3979ea1321c36a527f7664c
Ikmal Azman <opikmalazman@gmail.com>
2023-04-08 15:26:36 +0100

    create protocol for url session and make the HttpClient can be injectable with URLSessionContract type

--- NetworkRequest/HttpClient.swift
+++ NetworkRequest/HttpClient.swift
@@ -1,6 +1,6 @@
 //
 //  HttpClient.swift
-//  NetworkRequest
+//  Network-UnitTest
 //
 //  Created by Ikmal Azman on 08/04/2023.
 //
@@ -18,9 +18,19 @@ import Foundation
 final class HttpClient {
     typealias completeClosure = (_ data : Data?, _ error : Error?) -> Void
     
+    /// `session`,  environment to connect the app to internet
+    ///
+    /// `session`
+    /// - Can be injected when the `HttpClient` get created which allow to use different session in production and test environment
+    private let session : URLSessionContract
+    
+    init(session: URLSessionContract = URLSession.shared) {
+        self.session = session
+    }
+    
     func get(url : URL, completion : @escaping completeClosure ) {
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = session.dataTask(with: request) { data, response, error in
             completion(data, error)
         }
         task.resume()
